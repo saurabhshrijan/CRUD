7bb03b029aef886f563543537cb7d88b1b5b6110
imsaurabhshrijan1997@gmail.com <imsaurabhshrijan1997@gmail.com>
2020-03-10 23:20:32 +0530

    login-signup

--- controllers/login_controller.js
+++ controllers/login_controller.js
@@ -1,6 +1,22 @@
 const jwt=require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const s_key=require('../config');
+const User=require('../model/user');
 exports.login=(req,res,next)=>{
-    console.log(req.body);
-    res.status(200).json({"messege":req.body});
 
-}
+        User.findOne({ email: req.body.email }, function (err, user) {
+          if (err) return res.status(500).send('Error on the server.');
+          if (!user) return res.status(404).send('No user found.');
+          
+          let passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+          if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+          
+          var token = jwt.sign({ id: user._id }, s_key.secret, {
+            expiresIn: 86400 // expires in 24 hours
+          });
+          
+          res.status(200).send({ auth: true, token: token });
+        });
+        
+      
+    }
--- routes.js/login.route.js
+++ routes.js/login.route.js
@@ -4,8 +4,5 @@ const login_controller=require('../controllers/login_controller');
 
 router.post('/login',login_controller.login);
 
-router.get('/me',(req,res,next)=>{
-    res.status(200);
-})
 
 module.exports =router;
